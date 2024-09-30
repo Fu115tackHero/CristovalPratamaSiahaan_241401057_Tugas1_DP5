482a484fb2e1a1e63689fb31db210e91ac977c6b
Fu115tackHero <pratamacristoval@gmail.com>
2024-09-30 21:49:07 +0700

    ganjilGenap.pas

--- ganjilGenap.pas
+++ ganjilGenap.pas
@@ -9,11 +9,11 @@ clrscr;
     write('Masukkan Sebuah Angka: ');readln(angka);
 
     if (angka mod 2 = 0) then
-    begin
-        writeln('angka ', angka , ' merupakan bilangan genap')
-    end
+        begin
+            writeln('angka ', angka , ' merupakan bilangan genap')
+        end
     else
-    begin
-        writeln('angka ', angka , ' merupakan bilangan ganjil')
-    end;
+        begin
+            writeln('angka ', angka , ' merupakan bilangan ganjil')
+        end;
 end.
